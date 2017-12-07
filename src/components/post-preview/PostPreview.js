371f17f1da404d476e8a306228205f44d00aabbb
JM Myers <jm.myers@everbridge.com>
2017-12-07 01:39:58 -0500

    fix post preview source uri empty string warning

--- src/components/post-preview/PostPreview.js
+++ src/components/post-preview/PostPreview.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Dimensions, Image } from 'react-native';
 import styled from 'styled-components/native';
 import _ from 'lodash';
-import { MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons';
+import * as navigationConstants from 'constants/navigation';
 import Footer from './Footer';
 import Header from './Header';
 import BodyShort from './BodyShort';
@@ -48,14 +48,15 @@ class PostPreview extends Component {
     const { title, category, author, json_metadata, body, permlink, id } = postData;
     const parsedJsonMetadata = JSON.parse(json_metadata);
     const images = parsedJsonMetadata.image || [];
-
+    const previewImage = _.head(images);
+    const hasPreviewImage = images.length > 0 && !_.isEmpty(previewImage);
     return (
       <Container>
         <Header postData={postData} navigation={navigation} />
         <Content>
           <Touchable
             onPress={() =>
-              navigation.navigate('POST', {
+              navigation.navigate(navigationConstants.POST, {
                 title,
                 body,
                 permlink,
@@ -67,10 +68,10 @@ class PostPreview extends Component {
               })}
           >
             <Title>{title}</Title>
-            {images.length > 0 &&
+            {hasPreviewImage &&
               <PreviewImage
                 style={{ height: null, width }}
-                source={{ uri: _.head(images) }}
+                source={{ uri: previewImage }}
                 resizeMode={Image.resizeMode.cover}
               />}
             <BodyShort content={body} />
