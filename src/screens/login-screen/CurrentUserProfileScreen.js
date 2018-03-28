2de9bf2972ea72c95cc9af7e9203e0980f6b1207
JM Myers <jm.myers@everbridge.com>
2018-03-27 23:37:36 -0400

    remove unused modal

--- src/screens/login-screen/CurrentUserProfileScreen.js
+++ src/screens/login-screen/CurrentUserProfileScreen.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Modal } from 'react-native';
 import _ from 'lodash';
 import styled from 'styled-components/native';
 import {
